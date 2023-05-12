8e48dfb6437a5b63060081b1ee8733722715981f
John Doe <a.kykylin@gmail.com>
2023-05-12 19:42:46 +0400

    less2final

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
-const promoData = ["=)","*?*" ,")("]; 
+
+import testData from "./assents/data.json";
+import Card from "./components/Card/Card";
+import Header from "./components/Header";
+import { Footer } from "./components/Header";
+const promoData = ["=)", "*?*", ")("];
+
 const Promo = (props) => {
     return (
         <div className="promo__block">
@@ -11,13 +17,30 @@ const Promo = (props) => {
 
 
 const App = () => {
+    const smiles = []
+    for (let i = 0; i < promoData.length; i++) {
+        if (i % 2) {
+            smiles.push(<Promo key={`sm${i}`} text={promoData[i]}/>)
+        }
+    }
+
     return (
-        <div>
-            <h1>first page</h1>
-            <div className="container" >
-             {promoData.map(el => <Promo text={el}/>) }
+        <>
+            <Header />
+            <div>
+                <h1>first page</h1>
+                <div className="container" >
+                    {testData.map((pro, i) => (<Card key={i} img={pro.pictures}
+                        name={pro.name} price={pro.price} />))}
+                    {promoData.map(el => <Promo key={`${el}1`} text={el} />)}
+                    {promoData.map(el => <Promo key={`${el}2`} text={el} />)}
+                    <Promo text={promoData[1]} />
+
+                    <Promo text={promoData[2]} />
+                </div>
             </div>
-        </div>
+            <Footer />
+        </>
     )
 }
 
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+import "./style.css";
+export default () => {
+    return <header>
+      <div className="logo">DogFood</div>
+    
+    </header>
+}
+const Footer =()=><div> footer</div>
+export { Footer }
