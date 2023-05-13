ea781465a69820ed20654b1eedded25fa52f9a5b
John Doe <a.kykylin@gmail.com>
2023-05-13 10:56:49 +0400

    добавили bootstrapicons

--- src/components/General/Header.jsx
+++ src/components/General/Header.jsx
@@ -1,7 +1,25 @@
 import Logo from "./Logo";
+import {BalloonHeart,
+     Cart4, 
+     PersonCircle, 
+     BuildingUp,
+      BuildingDown, 
+     } from "react-bootstrap-icons";
 const Header = () => {
     return <header>
         <Logo />
+    <div className="search-block"></div>
+<nav className="header__menu">
+    <a href=""><BalloonHeart/></a>
+    <a href=""><Cart4/></a>
+    <a href=""><PersonCircle/></a>
+    <span>
+        <BuildingUp/>
+        <BuildingDown/>
+    </span>
+
+</nav>
+
     </header>
 }
 export default Header;
